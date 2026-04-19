9488a0e08c50f1f1d0f733c05fcab68b63607554
s5condlast-cmd <s5condlast@gmail.com>
2026-04-19 12:46:36 +0800

    revert admin changes to original state

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -38,7 +38,6 @@ const navSections = [
     items: [
       { label: "System Monitoring", href: "/admin/monitoring", icon: "M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" },
       { label: "Access Control", href: "/admin/access-control", icon: "M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" },
-      { label: "Data & Storyboard", href: "/admin/data-storyboard", icon: "M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" },
     ],
   },
 ];
